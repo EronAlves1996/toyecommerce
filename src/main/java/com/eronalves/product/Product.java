fe77d8cffd027c57eb1712d7d9efcfaa470e6edc
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-29 00:28:52 -0300

    fix: not persisting product

--- src/main/java/com/eronalves/product/Product.java
+++ src/main/java/com/eronalves/product/Product.java
@@ -2,8 +2,6 @@ package com.eronalves.product;
 
 import java.net.URI;
 
-import com.eronalves.product.ProductResource.ProductDTO;
-
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 import jakarta.ws.rs.core.UriInfo;
@@ -13,13 +11,6 @@ public class Product extends PanacheEntity {
 
   public String name;
   public int price;
-
-  public static Product of(ProductDTO product) {
-    var entity = new Product();
-    entity.name = product.name;
-    entity.price = product.price;
-    return entity;
-  }
 
   public URI informPath(UriInfo uriInfo) {
     return uriInfo.getAbsolutePathBuilder()
--- src/main/java/com/eronalves/product/ProductResource.java
+++ src/main/java/com/eronalves/product/ProductResource.java
@@ -2,9 +2,12 @@ package com.eronalves.product;
 
 import org.jboss.resteasy.reactive.RestResponse;
 
+import io.quarkus.hibernate.reactive.panache.Panache;
 import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.UriInfo;
@@ -16,19 +19,34 @@ import jakarta.ws.rs.core.UriInfo;
 public class ProductResource {
 
   static class ProductDTO {
-    String name;
-    int price;
+    public String name;
+    public int price;
+
+    Product toEntity() {
+      var entity = new Product();
+      entity.name = this.name;
+      entity.price = this.price;
+      return entity;
+    }
   }
 
   @POST
   @Produces(MediaType.APPLICATION_JSON)
   public Uni<RestResponse<Void>> createProduct(ProductDTO product,
       UriInfo uriInfo) {
-    return Product.of(product)
-        .persist()
+    return Panache.withTransaction(product.toEntity()::persist)
         .map(p -> (Product) p)
         .map(p -> p.informPath(uriInfo))
         .map(RestResponse::created);
+  }
+
+  @GET
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Uni<RestResponse<Product>> retrieve(@PathParam("id") long id) {
+    return Product.findById(id)
+        .map(p -> (Product) p)
+        .map(p -> RestResponse.ok(p));
   }
 
 }
