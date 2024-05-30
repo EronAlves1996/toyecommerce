5016de578a4bd9752a8973b02ca566276a780633
EronAlves1996 <eron.alves@rocketmail.com>
2024-05-30 14:41:06 -0300

    feat: migrate default roles

--- src/main/java/com/eronalves/user/Role.java
+++ src/main/java/com/eronalves/user/Role.java
@@ -1,7 +1,11 @@
 package com.eronalves.user;
 
+import org.hibernate.Session;
+
 import com.eronalves.persistence.DefaultEntity;
 
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import io.smallrye.mutiny.Uni;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
@@ -10,6 +14,21 @@ import jakarta.persistence.Entity;
  */
 @Entity
 public class Role extends DefaultEntity {
+
+  enum RoleType {
+    ADMIN(1), CUSTOMER(2);
+
+    public int id;
+
+    private RoleType(int id) {
+      this.id = id;
+    }
+  }
+
+  public static Uni<Role> from(RoleType role) {
+    return PanacheEntityBase.getSession()
+        .map(s -> s.getReference(Role.class, role.id));
+  }
 
   @Column(nullable = false, unique = true)
   public String name;
