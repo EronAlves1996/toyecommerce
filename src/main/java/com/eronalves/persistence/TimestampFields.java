59100a7fdf852164f7770edcd70c9f2ce1c24808
Eron Alves <eron.alves@letwe.com.br>
2024-06-04 17:06:30 -0300

    fix: timestamp fields

--- src/main/java/com/eronalves/persistence/TimestampFields.java
+++ src/main/java/com/eronalves/persistence/TimestampFields.java
@@ -2,6 +2,9 @@ package com.eronalves.persistence;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
@@ -12,9 +15,11 @@ import jakarta.persistence.Embeddable;
 public class TimestampFields {
 
   @Column(name = "created_at")
+  @CreationTimestamp
   public LocalDateTime createdAt;
 
   @Column(name = "updated_at")
+  @UpdateTimestamp
   public LocalDateTime updatedAt;
 
 }
