a6621870dce6f6ce15cc2a067dcd1d328097b923
EronAlves1996 <eron.alves@rocketmail.com>
2024-06-03 23:56:13 -0300

    feat: starts soft deletes

--- src/main/java/com/eronalves/persistence/SoftDeleteTimestampConverter.java
+++ src/main/java/com/eronalves/persistence/SoftDeleteTimestampConverter.java
@@ -1,6 +1,8 @@
 package com.eronalves.persistence;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
 
 import jakarta.persistence.AttributeConverter;
 
@@ -8,18 +10,20 @@ import jakarta.persistence.AttributeConverter;
  * SoftDeleteTimestampConverter
  */
 public class SoftDeleteTimestampConverter
-    implements AttributeConverter<Boolean, LocalDateTime> {
+    implements AttributeConverter<Boolean, Date> {
 
   @Override
-  public LocalDateTime convertToDatabaseColumn(Boolean isDeleted) {
+  public Date convertToDatabaseColumn(Boolean isDeleted) {
     if (isDeleted) {
-      return LocalDateTime.now();
+      return Date
+          .from(LocalDateTime.now().atZone(ZoneOffset.systemDefault())
+              .toInstant());
     }
     return null;
   }
 
   @Override
-  public Boolean convertToEntityAttribute(LocalDateTime deletedAt) {
+  public Boolean convertToEntityAttribute(Date deletedAt) {
     return deletedAt != null;
   }
 
--- src/main/java/com/eronalves/persistence/TimestampFields.java
+++ src/main/java/com/eronalves/persistence/TimestampFields.java
@@ -3,8 +3,6 @@ package com.eronalves.persistence;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 
 /**
  * TimestampFields
@@ -12,13 +10,10 @@ import jakarta.persistence.TemporalType;
 @Embeddable
 public class TimestampFields {
 
-  @Temporal(TemporalType.TIMESTAMP)
   public LocalDateTime createdAt;
 
-  @Temporal(TemporalType.TIMESTAMP)
   public LocalDateTime updatedAt;
 
-  @Temporal(TemporalType.TIMESTAMP)
   public LocalDateTime deletedAt;
 
 }
