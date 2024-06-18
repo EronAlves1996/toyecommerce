e2aa70c46513b048d7d9c5615e2bf783bf503811
EronAlves1996 <eron.alves@rocketmail.com>
2024-06-18 00:23:42 -0300

    feat: handle multipart form

--- src/main/java/com/eronalves/product/ProductResource.java
+++ src/main/java/com/eronalves/product/ProductResource.java
@@ -1,9 +1,14 @@
 package com.eronalves.product;
 
+import org.jboss.resteasy.reactive.PartType;
+import org.jboss.resteasy.reactive.RestForm;
 import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.multipart.FileUpload;
 
 import io.quarkus.hibernate.reactive.panache.Panache;
 import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.BeanParam;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -19,20 +24,27 @@ import jakarta.ws.rs.core.UriInfo;
 public class ProductResource {
 
   static class ProductDTO {
+    @RestForm
     public String name;
-    public int price;
+
+    @RestForm
+    public float price;
+
+    @RestForm
+    public FileUpload image;
 
     Product toEntity() {
       var entity = new Product();
       entity.name = this.name;
-      entity.price = this.price;
+      entity.price = (int) this.price * 100;
       return entity;
     }
   }
 
   @POST
   @Produces(MediaType.APPLICATION_JSON)
-  public Uni<RestResponse<Void>> createProduct(ProductDTO product,
+  @Consumes(MediaType.MULTIPART_FORM_DATA)
+  public Uni<RestResponse<Void>> createProduct(@BeanParam ProductDTO product,
       UriInfo uriInfo) {
     return Panache.withTransaction(product.toEntity()::persist)
         .map(p -> (Product) p)
